c572627ac76e81272c79ef571a4f91c9aad2f63a
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-09-13 20:46:02 -0700

    create homepage view

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,9 @@ const router = require('express').Router();
 const userRoutes = require('./api/user-routes');
 const postRoutes = require('./api/post-routes');
 const commentRoutes = require('./api/comment-routes');
+const homeRoutes = require('./home-routes.js');
+
+router.use('/', homeRoutes);
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
