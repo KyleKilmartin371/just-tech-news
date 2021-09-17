0d13a375045d8c357c721c75f49cb81e44f65e5e
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-09-17 14:50:31 -0700

    login functions

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const { User, Post, Vote } = require('../../models');
+const { User, Post, Comment, Vote } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
+    console.log(req.session);
     User.findAll({
-        attributes: { exclude: ['password']}
+        attributes: { exclude: ['password'] }
     })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
@@ -16,30 +17,30 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
-        attributes: { exclude: ['password']},
+        attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
         },
         include: [
             {
-              model: Post,
-              attributes: ['id', 'title', 'post_url', 'created_at']
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
             },
             {
                 model: Comment,
                 attributes: ['id', 'comment_text', 'created_at'],
                 include: {
-                  model: Post,
-                  attributes: ['title']
+                    model: Post,
+                    attributes: ['title']
                 }
-              },          
+            },
             {
-              model: Post,
-              attributes: ['title'],
-              through: Vote,
-              as: 'voted_posts'
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
             }
-          ]
+        ]
     })
         .then(dbUserData => {
             if (!dbUserData) {
@@ -52,7 +53,6 @@ router.get('/:id', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
-
 });
 
 // POST /api/users
@@ -62,20 +62,24 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
 });
 
-router.post('/login', (req, res) =>{
+router.post('/login', (req, res) => {
     User.findOne({
         where: {
             email: req.body.email
         }
     }).then(dbUserData => {
-        if (!dbUserData){
+        if (!dbUserData) {
             res.status(400).json({ message: 'No user with that email address!' });
             return;
         }
@@ -83,13 +87,30 @@ router.post('/login', (req, res) =>{
         const validPassword = dbUserData.checkPassword(req.body.password);
 
         if (!validPassword) {
-          res.status(400).json({ message: 'Incorrect password!' });
-          return;
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
         }
 
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
-    })
-})
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+          res.status(204).end();
+        });
+      }
+      else {
+        res.status(404).end();
+      }
+});
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
